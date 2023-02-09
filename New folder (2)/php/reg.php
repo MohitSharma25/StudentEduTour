a2891f06cf0ec904d1868a59db2b4cd8feee4ea0
adityaa75 <110570761+adityaa75@users.noreply.github.com>
2023-02-09 23:49:41 +0530

    database

---
+++ New folder (2)/php/reg.php
@@ -0,0 +1,54 @@
+<?php
+if (isset($_POST['submit'])) {
+    if (isset($_POST['email']) && isset($_POST['password']) &&
+        isset($_POST['cpassword']) ) {
+        
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+        $cpassword = $_POST['cpassword'];
+        
+        $host = "localhost";
+        $dbUsername = "root";
+        $dbPassword = "";
+        $dbName = "studentedutour";
+        $conn = new mysqli($host, $dbUsername, $dbPassword, $dbName);
+        if ($conn->connect_error) {
+            die('Could not connect to the database.');
+        }
+        else {
+            $Select = "SELECT email FROM registration WHERE email = ? LIMIT 1";
+            $Insert = "INSERT INTO registration(email, password, cpassword) values(?, ?, ?)";
+            $stmt = $conn->prepare($Select);
+            $stmt->bind_param("s", $email);
+            $stmt->execute();
+            $stmt->bind_result($resultEmail);
+            $stmt->store_result();
+            $stmt->fetch();
+            $rnum = $stmt->num_rows;
+            if ($rnum == 0) {
+                $stmt->close();
+                $stmt = $conn->prepare($Insert);
+                $stmt->bind_param("sss",$email, $password, $cpassword);
+                if ($stmt->execute()) {
+                    echo "New record inserted sucessfully.";
+                }
+                else {
+                    echo $stmt->error;
+                }
+            }
+            else {
+                echo "Someone already registers using this email.";
+            }
+            $stmt->close();
+            $conn->close();
+        }
+    }
+    else {
+        echo "All field are required.";
+        die();
+    }
+}
+else {
+    echo "Submit button is not set";
+}
+?>
